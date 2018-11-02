e90b27840a5c82f2c96657260f11ebcde041a0fa
Damian Mee <spam@meedamian.com>
2018-11-02 13:29:45 +0700

    simplify travis build

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,17 +6,14 @@ docker tag berkeleydb "$image:linux-arm-$TRAVIS_TAG"
 docker push "$image:linux-arm-$TRAVIS_TAG"
 
 set +e
-
 # this will probably never be necessary, as docker hub is fast and travis+qemu are **really** slow
-#   NOTE: yes, I don't like dependence on `stefanscherer/winspector`, but it stays for now.
 echo "Waiting for docker hub to finish building $image:linux-amd64-$TRAVIS_TAG"
-until docker run --rm stefanscherer/winspector "$image:linux-amd64-$TRAVIS_TAG"
-do
-    sleep 15
-    echo "Try again"
-done
-set -e
 
+if [[ "$(docker images -q "$image:linux-amd64-$TRAVIS_TAG" 2> /dev/null)" == "" ]]; then
+    sleep 15
+    echo "waiting for $image:linux-amd64-$TRAVIS_TAG to finish building…"
+fi
+set -e
 
 echo "Pushing manifest $image:$TRAVIS_TAG"
 docker -D manifest create "$image:$TRAVIS_TAG" \
@@ -26,11 +23,3 @@ docker -D manifest create "$image:$TRAVIS_TAG" \
 docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm --variant v6
 docker manifest push "$image:$TRAVIS_TAG"
 
-
-echo "Pushing manifest $image:latest"
-docker -D manifest create "$image:latest" \
-    "$image:linux-amd64-$TRAVIS_TAG" \
-    "$image:linux-arm-$TRAVIS_TAG"
-
-docker manifest annotate "$image:latest" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm --variant v6
-docker manifest push "$image:latest"
